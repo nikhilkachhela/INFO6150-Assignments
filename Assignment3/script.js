8aa36f630345ad4564842b507d1610f7a66cf4cf
Nikhil Kachhela <kachhela.n@northeastern.edu>
2025-09-23 01:25:36 -0400

    created new student row

--- Assignment3/script.js
+++ Assignment3/script.js
@@ -21,3 +21,44 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   }
 
+  // Create new student row
+  function addStudent() {
+    studentCount++;
+    const row = document.createElement("tr");
+
+    row.innerHTML = `
+      <td><input type="checkbox"></td>
+      <td>Student ${studentCount}</td>
+      <td>Teacher ${studentCount}</td>
+      <td>Subject ${studentCount}</td>
+      <td>${Math.floor(Math.random() * 100)}</td>
+      <td><img src="https://cdn-icons-png.flaticon.com/512/32/32195.png" class="arrow" width="20"></td>
+      <td></td>
+      <td></td>
+    `;
+
+    // Expandable hidden row
+    const expandRow = document.createElement("tr");
+    expandRow.classList.add("collapsed");
+    expandRow.innerHTML = `
+      <td colspan="8">Extra details for Student ${studentCount}</td>
+    `;
+
+    studentTable.appendChild(row);
+    studentTable.appendChild(expandRow);
+
+    alert(`Student ${studentCount} Record added successfully`);
+    attachRowEvents(row, expandRow);
+  }
+
+  function attachRowEvents(row, expandRow) {
+    const checkbox = row.querySelector("input[type='checkbox']");
+    const arrow = row.querySelector(".arrow");
+    const deleteCell = row.cells[6];
+    const editCell = row.cells[7];
+
+    checkbox.addEventListener("change", () => {
+      if (checkbox.checked) {
+        row.classList.add("yellow-row");
+
+
